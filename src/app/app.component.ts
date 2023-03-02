0c2e92695c673ee6482a56ed2c2efdbdd6713d97
wsalquinga <wilmer.alquinga@crsoft.org>
2023-03-02 17:39:02 -0500

    FEAT delete unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,6 @@ export class AppComponent implements OnInit {
 
   selectedProduct: Product = {} as Product;
 
-  displayAddDialog: boolean = false;
-
-  displayEditDialog: boolean = false;
-
-  displayDeleteDialog: boolean = false;
-
-  newProduct: Product = {} as Product;
-
   @ViewChild('dt') dt: Table | undefined;
 
   options: any;
@@ -46,55 +38,5 @@ export class AppComponent implements OnInit {
 
   getProducts(): void {
     this.productService.getProducts().subscribe(products => this.products = products);
-  }
-
-  showDialogToAdd(): void {
-    this.newProduct = {} as Product;
-    this.displayAddDialog = true;
-  }
-
-  save(): void {
-    this.productService.createProduct(this.newProduct).subscribe(() => {
-      this.messageService.add({severity:'success', summary:'Éxito', detail:'Producto agregado correctamente'});
-      this.getProducts();
-      this.displayAddDialog = false;
-    }, error => {
-      this.messageService.add({severity:'error', summary:'Error', detail:'Error al agregar el producto'});
-    });
-  }
-
-  edit(product: Product): void {
-    this.selectedProduct = {...product};
-    this.displayEditDialog = true;
-  }
-
-  update(): void {
-    this.productService.updateProduct(this.selectedProduct).subscribe(() => {
-      this.messageService.add({severity:'success', summary:'Éxito', detail:'Producto actualizado correctamente'});
-      this.getProducts();
-      this.displayEditDialog = false;
-    }, error => {
-      this.messageService.add({severity:'error', summary:'Error', detail:'Error al actualizar el producto'});
-    });
-  }
-
-  deleteConfirmation(product: Product): void {
-    this.selectedProduct = {...product};
-    this.confirmationService.confirm({
-      message: '¿Está seguro de que desea eliminar este producto?',
-      accept: () => {
-        this.delete();
-      }
-    });
-  }
-
-  delete(): void {
-    this.productService.deleteProduct(this.selectedProduct.product_id).subscribe(() => {
-      this.messageService.add({severity:'success', summary:'Éxito', detail:'Producto eliminado correctamente'});
-      this.getProducts();
-      this.displayDeleteDialog = false;
-    }, error => {
-      this.messageService.add({severity:'error', summary:'Error', detail:'Error al eliminar el producto'});
-    });
   }
 }
