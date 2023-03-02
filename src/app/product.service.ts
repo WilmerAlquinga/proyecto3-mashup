833f777a755b45d360186e7976ff7faf87ed3f5e
wsalquinga <wilmer.alquinga@crsoft.org>
2023-03-02 18:24:09 -0500

    FEAT adding apiBaseUrl by environment

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,30 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
+const apiUrl = environment.apiBaseUrl;
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
-  private baseUrl = 'http://localhost:3000/products';
+export class 
+ProductService {
+  // private baseUrl = 'http://localhost:3000/products';
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl);
-  }
-
-  createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
-  }
-
-  updateProduct(product: Product): Observable<Product> {
-    const url = `${this.baseUrl}/${product.product_id}`;
-    return this.http.put<Product>(url, product);
-  }
-
-  deleteProduct(productId: number): Observable<void> {
-    const url = `${this.baseUrl}/${productId}`;
-    return this.http.delete<void>(url);
+    return this.http.get<Product[]>(apiUrl);
   }
 }
